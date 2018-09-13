d8b0eabbd16fd504699e9dc2c0cd2a3638dbb144
janginwoo <inustar98@naver.com>
2018-09-13 18:59:16 +0900

    다시했습니다
    https://www.youtube.com/watch?v=t6BocXu1Vds

--- drill4/character_runs.py
+++ drill4/character_runs.py
@@ -1,8 +1,8 @@
 from pico2d import *
 open_canvas()
 grass = load_image('grass.png')
-character = load_image('run_animation.png')
-
+character2 = load_image('animation_sheet.png')
+character1 = load_image('run_animation.png')
 x = 0
 
 x = 0
@@ -12,23 +12,23 @@ while(True):
     while boy == 0:
         clear_canvas()
         grass.draw(400, 30)
-        character.clip_draw(frame*100, 0, 100, 100, x, 90)
+        character1.clip_draw(frame*100, 0, 100, 100, x, 90)
         update_canvas()
         frame = (frame+1) % 8
         x += 5
-        if x > 799:
-            boy=1
+        if(x > 800):
+            boy = 1
         delay(0.05)
         get_events()
     while boy == 1:
         clear_canvas()
         grass.draw(400, 30)
-        character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+        character2.clip_draw(frame * 100, 0, 100, 100, x, 90)
         update_canvas()
         frame = (frame + 1) % 8
         x -= 5
-        if x < 0:
-            boy = 0
+        if(x<0):
+            boy=0
         delay(0.05)
         get_events()
 
