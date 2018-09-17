83a3f1f62e3517387c2958120f9936598f99a288
janginwoo <inustar98@naver.com>
2018-09-17 16:22:21 +0900

    drill05up
    up만들기

--- drill5/drill05.py
+++ drill5/drill05.py
@@ -13,6 +13,16 @@ def move_from_center_to_right():
         x += 2
         delay(0.01)
 def move_up():
+    x, y = 800 - 25, 90
+    while x < 600-50:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now_(x, y)
+        y += 2
+        delay(0.01)
+
+
+def move_up():
     pass
 def move_left():
     pass
@@ -21,7 +31,7 @@ def move_down():
 def move_from_left_to_center():
     pass
 def make_rectangle() :
-    move_from_center_to_right()
+    #move_from_center_to_right()
     move_up()
     move_left()
     move_down()
