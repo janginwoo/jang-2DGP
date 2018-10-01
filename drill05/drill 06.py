a3be48cc5851381135910f1216f007a5a375b6bf
janginwoo <inustar98@naver.com>
2018-10-01 16:56:28 +0900

    시작1

--- drill05/drill 06.py
+++ drill05/drill 06.py
@@ -5,13 +5,17 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 def handle_events():
     global running
-    global x, y_
+    global x, y
+    global mx,my
+
     events = get_events()
     for event in events:
+        if event.type == SDL_MOUSEMOTION:
+            hand_arrow.draw(event.x, KPU_HEIGHT-1-event.y)
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEBUTTONDOWN:
-            x,y = event.x, KPU_HEIGHT -1 - event.y
+            x,y = event.x, event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 open_canvas(KPU_HEIGHT, KPU_WIDTH)
@@ -27,7 +31,7 @@ while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2 , KPU_HEIGHT // 2 )
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
-    hand_arrow
+
     update_canvas()
     frame = (frame + 1) % 8
 
